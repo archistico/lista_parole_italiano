1feeddabe7b49657970cc321805fe50987d99068
archistico <info@archistico.com>
2022-05-27 18:57:06 +0200

    add libro

--- aggiungi.py
+++ aggiungi.py
@@ -8,7 +8,7 @@ def pulisci(value):
 f = open("nuove_parole_non_inserite.txt", mode="r", encoding="utf-8")
 for x in f:
     parola = pulisci(x)
-    if "'" not in parola:
+    if "'" not in parola and parola!="":
         cur.execute(f'INSERT INTO parole VALUES ("{parola}",0)')
         print(parola)
 f.close()
